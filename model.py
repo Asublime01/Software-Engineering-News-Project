a6f1dc135ecb59e2dd2b0aa5e8ba9c5e3a6b1941
Asublime01 <cbtech4581@gmail.com>
2025-10-29 15:11:13 -0600

    Day 1 kinda

--- model.py
+++ model.py
@@ -11,14 +11,28 @@ nyt_apiKey = os.getenv("NYT_API_KEY")
 nyt_endpoint = f'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key={nyt_apiKey}'
 
 
-responseNYT = requests.get(nyt_endpoint)
-responseNYT_parsed = responseNYT.json()
+#responseNYT = requests.get(nyt_endpoint)
+responseCNN = requests.get("https://www.cnn.com/business/tech")
 
-with open("response.json", "w") as file:
-    json.dump(responseNYT_parsed, file, indent=2)
+#responseNYT_parsed = responseNYT.json()
+CNN_html = responseCNN.content
 
-print(responseNYT_parsed)
 
-#soup = BeautifulSoup(html, 'html.parser')
+
+#with open("response.json", "w") as file:
+#    json.dump(responseNYT_parsed, file, indent=2)
+
+#print(responseNYT_parsed)
+
+soup = BeautifulSoup(CNN_html, 'html.parser')
+
+outerdivs = soup.find_all('div',{'class': 'zone zone--t-light', "data-collapsed-text": ""})
+
+for div in outerdivs:
+    soup = div
+    spans = soup.find_all("span", {"class": "container__headline container_vertical-strip__headline"})
+    print(spans)
+
+
 
 

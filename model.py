349f7dd3257120e800e38374ef2790f82353c337
Asublime01 <99103839+Asublime01@users.noreply.github.com>
2025-11-05 14:01:12 -0700

    Add random news gathering

--- model.py
+++ model.py
@@ -5,20 +5,29 @@ import os
 from dotenv import load_dotenv
 import json
 import feedparser
+import random
 
 load_dotenv()
 nyt_apiKey = os.getenv("NYT_API_KEY")
 thenews_apiKEY = os.getenv("thenews_apiKEY")
+headers = {"User-Agent": "Mozilla/5.0"}
 
 nyt_endpoint = f'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key={nyt_apiKey}'
 
-guardian_url = 'https://www.theguardian.com/us/technology/rss' 
+guardian_url = 'https://www.theguardian.com/technology/rss' 
 hacker_news = 'https://hnrss.org/frontpage'
 
-guardian_feed = feedparser.parse(guardian_url)
+guardian_response = requests.get(guardian_url, headers=headers)
+guardian_feed = feedparser.parse(guardian_response.text)
 responseNYT = requests.get(nyt_endpoint)
 print(responseNYT)
-hacker_feed = feedparser.parse(hacker_news)
+hacker_response = requests.get(hacker_news, headers=headers)
+hacker_feed = feedparser.parse(hacker_response.text)
+
+
+
+
+
 
 
 responseNYT_parsed = responseNYT.json()
@@ -28,25 +37,45 @@ responseNYT_parsed = responseNYT.json()
 nyt_data = responseNYT_parsed["results"]
 
 with open("News.txt", "w") as file:
-    file.write("---------------------HACKER NEWS-----------------------------\n\n")
     for entry in hacker_feed.entries:
-        file.write(f"Title: {entry.title}\nLink: {entry.link}\n\n")
-    file.write("---------------------New York Times News-----------------------------\n\n")
+        file.write(f"[{entry.title}, {entry.link}]\n")
+    
     for result in nyt_data:
-        file.write(f"Title: {result["title"]}\nDescription: {result["abstract"]}\nUrl: {result["url"]}\n\n")
-    file.write("---------------------Guardian Tech News-----------------------------\n\n")    
+        file.write(f"[{result['title']}, {result['abstract']}, {result['url']}\n")
+       
     for entry in guardian_feed.entries:
-        file.write(f"Title: {entry.title}\nLink: {entry.link}\n\n")
+        file.write(f"[{entry.title}, {entry.link}]\n")
 
 
 
 
-with open("response.json", "w") as file:
-    json.dump(responseNYT_parsed, file, indent=2)
+
+def send_newsletter():
+    with open("News.txt", "r") as file:
+        lines = file.readlines()
+        lines_chosen = []
+        gathered_data = []
+        while len(lines_chosen) < 5:
+            chosen_line  = random.choice(lines)
+            line_number = lines.index(chosen_line) + 1
+            if line_number in lines_chosen:
+                continue #Find a new line that hasn't been picked
+            else:
+                lines_chosen.append(line_number) #Add that new line to the lines picked list
+                gathered_data.append(chosen_line)
+        
+        
+
+        
+
+def get_random_news():
+    pass
+
+
+send_newsletter()
 
 
 
-print("Finished.")
 
 
 
